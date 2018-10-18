04fa9d3778cf148a9bb1bb50f76130449088b3b2
Jessiely Oliveira <jessiely.costa@gmail.com>
2018-10-18 12:09:18 -0300

    Estilizando jogo

--- projeto_estourando_baloes/jogo.js
+++ projeto_estourando_baloes/jogo.js
@@ -1,24 +1,60 @@
-function iniciaJogo () {
+// Armazena a chamada da função timeOut
+var timerId = null; 
+
+function iniciaJogo() {
 	var url = window.location.search;
 	var nivel = url.replace("?", "");
-	var tempo_s = 0;
+	var tSegundos = 0;
 	if(nivel == 1) {
-		tempo_s = 120;
+		tSegundos = 120;
 	} 
 	if (nivel == 2) {
-		tempo_s = 60;
+		tSegundos = 60;
 	}
 	if (nivel == 3) {
-		tempo_s = 30;
+		tSegundos = 3;
 	}
 	
 	// Inserindo segundos no span
-	document.getElementById('tempo_jogo').innerHTML = tempo_s;
+	document.getElementById('tempo_jogo').innerHTML = tSegundos;
 
-	// Parei aqui
+	// Quantidade de balões
+	var qtdeBaloes = 50;
+	criaBaloes(qtdeBaloes);
+
+	// Imprimir quantidade de balões inteiros
+	document.getElementById('baloes_inteiros').innerHTML = qtdeBaloes;
+	// Imprimir quantidade de balões estourados
+	document.getElementById('baloes_estourados').innerHTML = 0;
+
+	contagemTempo(tSegundos + 1);
 }
 
 
-function criaBaloes () {
+function criaBaloes(qtdeBaloes) {
+	for (var i = 1; i <= qtdeBaloes; i++) {
+		var balao = document.createElement("img");
+		balao.src = 'imagens/balao_azul_pequeno.png';
+		balao.style.margin = '10px';
 
+		document.getElementById('cenario').appendChild(balao);
+	}
+}
+
+function contagemTempo(segundos) {
+	segundos = segundos - 1;
+
+	if(segundos == -1) {
+		clearTimeout(timerId);
+		gameOver();
+		return false;
+	}
+
+	document.getElementById('tempo_jogo').innerHTML = segundos;
+	timerId = setTimeout("contagemTempo("+segundos+")", 1000);
+}
+
+function gameOver() {
+	document.getElementById('game_over').style.color = 'black';
+	document.getElementById('game_over').style.textShadow = 'white 0.1em 0.1em 0.1em';
 }
