61d206c32c2bba0f407a59b57fbd2e9d36526aba
Jessiely Oliveira Costa <jessiely.oliveira@A3032012017602L.imd.local>
2018-10-31 09:12:52 -0300

    Alterações layout

--- projeto-jogo-velha/jogo.js
+++ projeto-jogo-velha/jogo.js
@@ -3,8 +3,15 @@ $(document).ready( function() {
 	$('#palcoJogo').hide();
 	
 	$('#iniciar').click( function() {
+
 		var jogador1 = document.getElementById('jogador1').value;
 		var jogador2 = document.getElementById('jogador2').value;
+
+		if(jogador2 == "" || jogador2 == "") {
+			document.getElementById('msgErro').innerHTML = "Insira os nomes dos jogadores!";
+
+			return false;			
+		}
 
 		$('#telaInicial').hide();
 		$('#palcoJogo').show();
