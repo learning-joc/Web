50e808d71be57edc237c863947555fd24cf96f72
Jessiely Oliveira <jessiely.costa@gmail.com>
2018-11-01 12:07:15 -0300

    Tentando inserir os icones de jogada

--- projeto-jogo-velha/jogo.js
+++ projeto-jogo-velha/jogo.js
@@ -1,9 +1,12 @@
+var rodada = 1;
+var matrizJogo = new Array(3);
+
 $(document).ready( function() {
 	
-	$('#iniciar').click( function () {
+	$('#iniciar').click( function() {
 
 			// Validar entradas
-			if(!validarEntradasNomes()) {
+			if(!validaEntradasNomes()) {
 				return false;
 			}
 
@@ -11,19 +14,41 @@ $(document).ready( function() {
 			$('#nomeJogador1').html($('#jogador1').val());
 			$('#nomeJogador2').html($('#jogador2').val());
 
+			//$('#msg').html($('#jogador1').val());
 			$('#palcoJogo').show();
 			$('#telaInicial').hide();
+			
+	});
 
-
-	})
+	$('.jogada').click( function() {
+		var campoId = this.id;
+		realizaJogada(campoId);
+	});
 
 } )
 
 
-function validarEntradasNomes() {
+function realizaJogada(id) {
+	var icone = '';
+	var ponto = 0;
+	if((rodada % 2) == 1) {
+		$('#msg').html($('#jogador1').val());
+		ponto = -1;
+		icone = 'imagens/marcacao_1.png';
+	} else {
+		$('#msg').html($('#jogador2').val());
+		ponto = 1;
+		icone = 'imagens/marcacao_2.png';
+	}
+	rodada++;
+}
+
+
+function validaEntradasNomes() {
 	if($('#jogador1').val() == "" && $('#jogador2').val() == "") {
 		$('#jogador1').css("border", "1px solid red");
 		$('#jogador2').css("border", "1px solid red");
+
 		return false;
 	}
 	if($('#jogador1').val() == "") {
